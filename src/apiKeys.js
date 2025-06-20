86936511f555fac0351bf9031710f32b497326e6
Anurag-cse <122852810+Anuraga3s@users.noreply.github.com>
2025-06-20 16:41:11 +0530

    Update and rename apiKeys.js to src/apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "f6849d30e9653f372e741612085893ab",
   base: "https://api.openweathermap.org/data/2.5/",
 };
